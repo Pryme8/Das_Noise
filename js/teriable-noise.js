20207346cc24cfc5bb4eee7f2e981b09927397c4
Andrew Butt <pryme8@gmail.com>
2016-08-17 17:36:43 -0700

    Update
    Moving over to a Clean constructor... got some samples up.

--- js/teriable-noise.js
+++ js/teriable-noise.js
@@ -1,124 +1,326 @@
 //Andrew V Butt Sr. - Pryme8@gmail.com
 //Compilation of Standard Noises for Javascript version 1.1.0;
 //Pryme8.github.io
-
-function WorleyNoise2D(numPoints, seed, width, height, targetPoint, style){
-this._numPoints = numPoints || 1,
-this._keyPoints = [],
-this._seed = parseInt(seed) || 3241,
-this._width = width || 1,
-this._height = height || 1;
-this._targetPoint = targetPoint || 2;
-this._style = window[style] || euclidean;
-
-this._getKeyPoints();
-
-this._normalValues = [];
-
-}
-
-
-WorleyNoise2D.prototype._getKeyPoints = function(){
-	for(var i = 0; i < this._numPoints; i++){
-		this._keyPoints.push({x: WorleyNoise2D.Random(this._seed/((i+1)/2)),y: WorleyNoise2D.Random(((this._seed+1)+(i+1)/2)/2)});	
-	}
-	this._ApplyKeyPointsToMap();
-	return;
-};
-
-WorleyNoise2D.Random = function(seed){
-	var s = Math.sin(seed) * 10000;
-	return s - Math.floor(s);	
-};
-
-WorleyNoise2D.prototype._ApplyKeyPointsToMap = function(){
-	console.log("width: "+this._width+" - height: "+ this._height);
-	 for(var i = 0; i < this._keyPoints.length; i++){
-		 
-		 this._keyPoints[i].x = (this._width*-0.5)+(this._width * this._keyPoints[i].x);
-		 this._keyPoints[i].y = (this._height*-0.5)+(this._height * this._keyPoints[i].y);
-		 //this._keyPoints[i].x = (this._width * this._keyPoints[i].x)-(this._width/2);
-		 //this._keyPoints[i].y = (this._height * this._keyPoints[i].y)-(this._height/2);
-	}
-	return;
-};
-
-WorleyNoise2D.prototype._calculateDistance = function(ix, iy){
-	var dist;
-
-	for(var i = 0; i < this._keyPoints.length; i++){
-		dist = this._style(ix - this._keyPoints[i].x, iy - this._keyPoints[i].y);
-		this._normalValues.push(dist);
-	}
-	this._normalValues.sort(function(a, b){return a-b});	
-};
-
-WorleyNoise2D.prototype._getPointValue = function(ix, iy){
-		this._calculateDistance(ix, iy);
-		
-		var scale = 1 / (this._normalValues[this._normalValues.length-1] - this._normalValues[0]);
-			var value = ((this._normalValues[this._targetPoint] - this._normalValues[0]) * scale)/0.1;
-			
-		value = Math.max(-1, Math.min(1, (value * 2) - 1));
-		
+Teriable = {} || Teriable;
+Teriable.Noise = function(type,seed,args){
+	if(this._type){this._type = type}else{this._type = "Simple2"};
+	this._seed = {_initial : seed,
+				  _clean : this._cleanSeed(seed)};
+	this.args = args;
+	console.log("New "+type+" Init: seed._initial-"+this._seed._initial+", seed._clean-"+this._seed._clean);
 	
-		this._normalValues = [];
-		return value;
+	switch (this._type) {
+	case "Simple2": this.Simple2(); break;
+	}
 }
 
-function euclidean(dx, dy){
-	return dx * dx + dy * dy;	
+Teriable.Noise.prototype._cleanSeed = function(seed){
+	var t=0;
+	seed+="";
+	for(var i = 0; i < seed.length; i++){
+	t+= seed.charCodeAt(i);
+	}
+	seed = t;	
+	var s = Math.sin(seed) * 10000;
+	return parseFloat(s - Math.floor(s));
+};
+
+Teriable.Noise.Grad = function(x, y, z) {
+    this.x = x; this.y = y; this.z = z;
+};
+
+Teriable.Noise.Grad.prototype.dot2 = function(x, y) {
+    return this.x*x + this.y*y;
+};
+
+Teriable.Noise.Grad.prototype.dot3 = function(x, y, z) {
+    return this.x*x + this.y*y + this.z*z;
+};
+
+Teriable.Noise._CreateGrad = function(){
+	var Grad = Teriable.Noise.Grad;
+	return [new Grad(1,1,0),new Grad(-1,1,0),new Grad(1,-1,0),new Grad(-1,-1,0),
+               new Grad(1,0,1),new Grad(-1,0,1),new Grad(1,0,-1),new Grad(-1,0,-1),
+               new Grad(0,1,1),new Grad(0,-1,1),new Grad(0,1,-1),new Grad(0,-1,-1)];
 }
 
-function manhattan(dx, dy) {
+
+Teriable.Noise.prototype.getValue = function(args){
+	switch (this._type) {
+	case "Simple2": 
+	if(typeof args.x == 'undefined' && typeof args.y == 'undefined'){return "Error Please input {x:?,y:?}"}
+	return this.Simple2_Get(args.x, args.y); 
+	break;
+	}
+};
+
+Teriable.Noise.prototype.Simple2 = function(){
+this.grad3 = new Teriable.Noise._CreateGrad();
+this.p = new Teriable.Noise.Const._p();
+var temp = new Teriable.Noise.gradPerm(this._seed._clean,new Array(512),new Array(512), this.p, this.grad3);
+this.perm = temp.perm;
+this.gradP = temp.gradP;
+};
+
+
+Teriable.Noise.prototype.Simple2_Get = function(xin, yin){
+	if(typeof this.args.scale !== 'undefined' && this.args.scale != 0){
+		xin = Math.floor(xin/this.args.scale);
+		yin = Math.floor(yin/this.args.scale);
+	}
+	
+	
+	
+	var F2 = Teriable.Noise.Const._F2,
+	    G2 = Teriable.Noise.Const._G2,
+		F3 = Teriable.Noise.Const._F3,
+		G3 = Teriable.Noise.Const._G3,
+		n0, n1, n2; 
+    	s = (xin+yin)*F2,
+    	i = Math.floor(xin+s),
+    	j = Math.floor(yin+s),
+    	t = (i+j)*G2,
+    	y0 = yin-j+t,
+		x0 = xin-i+t;
+	//console.log("s:"+s);	
+	//console.log("i:"+i);	
+	//console.log("j:"+j);
+	//console.log("t:"+t);
+    //
+	//console.log("x0:"+x0);
+	//console.log("y0:"+y0);
+	
+	
+    var i1, j1;
+    if(x0>y0) {
+      i1=1; j1=0;
+    } else {
+      i1=0; j1=1;
+    }
+    var x1 = x0 - i1 + G2, 
+	
+    	y1 = y0 - j1 + G2,
+		
+    	x2 = x0 - 1 + 2 * G2,
+		
+    	y2 = y0 - 1 + 2 * G2;
+	//console.log("x1:"+x1);
+	//console.log("y1:"+y1);
+	//console.log("x2:"+x2);
+	//console.log("y2:"+y2);
+    i &= 255,
+    j &= 255;
+	//console.log("i:"+i);
+	//console.log("j:"+j);
+    var gi0 = this.gradP[i+this.perm[j]];
+	//console.log("gi0:"),console.log(gi0);
+    var gi1 = this.gradP[i+i1+this.perm[j+j1]];
+	//console.log("gi1:"),console.log(gi1);
+    var gi2 = this.gradP[i+1+this.perm[j+1]];
+	//console.log("gi2:"),console.log(gi2);
+
+    var t0 = 0.5 - x0*x0-y0*y0;
+	//console.log("t0:"+t0);
+    if(t0<0) {
+      n0 = 0;
+    } else {
+      t0 *= t0;
+      n0 = t0 * t0 * gi0.dot2(x0, y0); 
+    }
+	//console.log("t0:"+t0);
+	//console.log("n0:"+n0);
+	
+    var t1 = 0.5 - x1*x1-y1*y1;
+	//console.log("t1:"+t1);
+    if(t1<0) {
+      n1 = 0;
+    } else {
+      t1 *= t1;
+      n1 = t1 * t1 * gi1.dot2(x1, y1);
+    }
+	//console.log("t1:"+t1);
+	//console.log("n1:"+n1);
+	
+    var t2 = 0.5 - x2*x2-y2*y2;
+    if(t2<0) {
+      n2 = 0;
+    } else {
+      t2 *= t2;
+      n2 = t2 * t2 * gi2.dot2(x2, y2);
+    }
+	
+	//console.log("t2:"+t2);
+	//console.log("n2:"+n2);
+	var value = (70 * (n0 + n1 + n2)+1)/2;
+	
+    return value;
+}
+
+
+Teriable.Noise.gradPerm = function(seed, gradP, perm, p, grad3){
+	if(seed > 0 && seed < 1) {
+      // Scale the seed out
+      seed *= 65536;
+    }
+
+    seed = Math.floor(seed);
+    if(seed < 256) {
+      seed |= seed << 8;
+    }
+
+    for(var i = 0; i < 256; i++) {
+      var v;
+      if (i & 1) {
+        v = p[i] ^ (seed & 255);
+      } else {
+        v = p[i] ^ ((seed>>8) & 255);
+      }
+
+      perm[i] = perm[i + 256] = v;
+      gradP[i] = gradP[i + 256] = grad3[v % 12];
+    }
+	
+	this.perm = perm;
+	this.gradP = gradP;	
+	
+	
+	
+};
+
+Teriable.Noise.Const= {};
+Teriable.Noise.Const._p = function(){return [151,160,137,91,90,15,
+  131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,8,99,37,240,21,10,23,
+  190, 6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,35,11,32,57,177,33,
+  88,237,149,56,87,174,20,125,136,171,168, 68,175,74,165,71,134,139,48,27,166,
+  77,146,158,231,83,111,229,122,60,211,133,230,220,105,92,41,55,46,245,40,244,
+  102,143,54, 65,25,63,161, 1,216,80,73,209,76,132,187,208, 89,18,169,200,196,
+  135,130,116,188,159,86,164,100,109,198,173,186, 3,64,52,217,226,250,124,123,
+  5,202,38,147,118,126,255,82,85,212,207,206,59,227,47,16,58,17,182,189,28,42,
+  223,183,170,213,119,248,152, 2,44,154,163, 70,221,153,101,155,167, 43,172,9,
+  129,22,39,253, 19,98,108,110,79,113,224,232,178,185, 112,104,218,246,97,228,
+  251,34,242,193,238,210,144,12,191,179,162,241, 81,51,145,235,249,14,239,107,
+  49,192,214, 31,181,199,106,157,184, 84,204,176,115,121,50,45,127, 4,150,254,
+  138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180];
+};
+
+Teriable.Noise.Const._F2 = 0.5*(Math.sqrt(3)-1);
+Teriable.Noise.Const._G2 = (3-Math.sqrt(3))/6;
+Teriable.Noise.Const._F3 = 1/3;
+Teriable.Noise.Const._G3 = 1/6;
+
+
+
+
+
+
+/*
+Teriable.Noise.prototype.Worley2 = function(){
+	console.log("Worley GO!");
+	if(!this.args.nPoints){this.args.nPoints = 20;}
+	if(!this.args.width){this.args.width = 100;}
+	if(!this.args.height){this.args.height = 100;}
+	if(!this.args.scale){this.args.scale = 100;}
+	if(!this.args.n){this.args.n = 4;}
+	if(!this.args.style){this.args.style = 'euclidean';}
+	console.log(this.args);
+	this.data = {keyPoints : []};
+	
+	this._getKeyPoints();
+	this._ApplyKeyPointsToMap();
+	console.log(this);
+};
+
+Teriable.Noise.prototype._getKeyPoints = function(){
+	for(var i = 0; i < this.args.nPoints; i++){
+		this.data.keyPoints.push({x:this._cleanSeed((1+(i*(i*0.5)))*this._seed._clean),y:this._cleanSeed((1 + (i+i))*(this._seed._clean*0.95))});	
+	}
+};
+
+Teriable.Noise.prototype._ApplyKeyPointsToMap = function(){
+	 for(var i = 0; i < this.data.keyPoints; i++){		 
+		this.data.keyPoints[i].x = (this.args.width*-0.5)+(this.args.width * this._keyPoints[i].x);
+		this.data.keyPoints[i].y = (this.args.height*-0.5)+(this.args.height * this._keyPoints[i].y);
+	}
+};
+
+Teriable.Noise.prototype.getValue = function(loc){
+	switch (this._type) {
+	case "Worley2": return this._Worly2D_Calc(loc); break;
+	}
+};
+
+Teriable.Noise.prototype._Worly2D_Calc = function(loc){
+		var dist;
+		var ix = loc.x, iy = loc.y;
+		this.data.normalValues = [];
+		for(var i = 0; i < this.data.keyPoints.length; i++){
+		dist = this.Worley2[this.args.style](ix - this.data.keyPoints[i].x, iy - this.data.keyPoints[i].y);
+		this.data.normalValues.push(dist);
+		}
+		this.data.normalValues.sort(function(a, b){return a-b});
+
+		this.data.value = ((this.data.normalValues[this.args.n] - this.data.normalValues[0]));
+		
+		this.data.value*=this.args.scale;
+		
+		return this.data.value;
+};
+
+Teriable.Noise.prototype.Worley2.euclidean = function(dx, dy){
+	return Math.abs(Math.sqrt((dy - dx) * (dy - dx)));	
+}
+
+Teriable.Noise.prototype.Worley2.manhattan = function(dx, dy) {
     return Math.abs(dx) + Math.abs(dy);
 }
 
-function manhattan2(dx, dy){
+Teriable.Noise.prototype.Worley2.manhattan2 = function(dx, dy){
 	return Math.abs(dx) - Math.abs(dy);	
 }
 
-function euclidean2(dx, dy){
+Teriable.Noise.prototype.Worley2.euclidean2 = function(dx, dy){
 	return dx * dx - dy * dy;	
 }
 
-function chebyshevish(dx,dy){
+Teriable.Noise.prototype.Worley2.chebyshevish = function(dx,dy){
 	return Math.max(Math.abs(dx - (dy/2)),Math.abs(dy - (dx/2)));
 }
 
-function chebyshevish2(dx,dy){
+Teriable.Noise.prototype.Worley2.chebyshevish2 = function(dx,dy){
 	return Math.min(Math.abs(dx + (dy/2)),Math.abs(dy + (dx/2)));
 }
 
-function chebyshevish3(dx,dy){
+Teriable.Noise.prototype.Worley2.chebyshevish3 = function(dx,dy){
 	return Math.min(Math.abs(dx - (dy/2)),Math.abs(dy - (dx/2)));
 }
 
-function chebyshevish4(dx,dy){
+Teriable.Noise.prototype.Worley2.chebyshevish4 = function(dx,dy){
 	return Math.max(Math.abs(dx + (dy/2)),Math.abs(dy + (dx/2)));
 }
 
-function valentine(dx, dy){
+Teriable.Noise.prototype.Worley2.valentine = function(dx, dy){
 	return Math.min((dx/dy),(dy/dx))/Math.max((dx/dy),(dy/dx));
 }
 
-function valentine2(dx, dy){
+Teriable.Noise.prototype.Worley2.valentine2 = function(dx, dy){
 	return Math.abs(Math.min((dx/-dy),(dy/-dx))/Math.max((dx/-dy),(dy/-dx)));
 }
 
-function valentine3(dx, dy){
+Teriable.Noise.prototype.Worley2.valentine3 = function(dx, dy){
  	var r = (dx+dy) * Math.cos(Math.min(dx,dy)/Math.max(dx,dy));
 	var r2 = (dx+ dy) * Math.sin(Math.min(dx,dy)/Math.max(dx,dy));
 	
 	return Math.abs((dx - r) + (dy - r2));
 }
 
-function valentine4(dx, dy){
+Teriable.Noise.prototype.Worley2.valentine4 = function(dx, dy){
  	var r = (dx+dy) * Math.cos(Math.min(dx,dy)/Math.max(dx,dy));
 	var r2 = (dx+ dy) * Math.sin(Math.min(dx,dy)/Math.max(dx,dy));
 	
 	return Math.abs(Math.min((dx - r) / (dy - r2),(dx + r) / (dy + r2) ));
 }
+
+
 
 function sNoise2(xy, seed){
 	var grad3 = [new Grad(1,1,0),new Grad(-1,1,0),new Grad(1,-1,0),new Grad(-1,-1,0),
@@ -569,7 +771,7 @@ Grad.prototype.dot3 = function(x, y, z) {
 
 
 
-
+*/
 
 
 
